1b45a5c8efad4bbaadae6aa5c4219a7cd0142074
divnaoki <naokiimai1@gmail.com>
2023-12-04 09:04:10 +0900

    12/4 画像ファイル以外はスキップ処理追加

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ image_dirname = dirname + "/image/"
 # 画像ファイルが存在しない場合、エラー出力実施
 
 # 画像ファイル取得、テキスト化し出力
-if not any(filename.endswith(('.png', '.jpg')) for filename in os.listdir(image_dirname)):
-  print("対象ファイルが存在しません。処理を終了します。")
-else:
-  for filename in os.listdir(image_dirname):
-    img_path = os.path.join(image_dirname, filename)
-    img = Image.open(img_path)
-    # Use tesseract to do OCR on the image
-    text = pytesseract.image_to_string(img, lang='jpn')
-    # Print the text from each image
-    print(f'対象ファイル：{filename}:\n{text}\n')
-    print("================================================")
+for filename in os.listdir(image_dirname):
+    if filename.endswith(('.png', '.jpg')):
+        img_path = os.path.join(image_dirname, filename)
+        try:
+            img = Image.open(img_path)
+            # Use tesseract to do OCR on the image
+            text = pytesseract.image_to_string(img, lang='jpn')
+            # Print the text from each image
+            print(f'対象ファイル：{filename}:\n{text}\n')
+            print("================================================")
+        except IOError:
+            print(f'ファイル{filename}を開けませんでした。')
+    else:
+        print(f'非画像ファイル{filename}は無視されました。')
