49c1034646095227a1befd7188eae842b522d66c
divnaoki <naokiimai1@gmail.com>
2023-12-10 16:13:38 +0900

    12/10 画像取得順に表示するよう修正

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import pytesseract
 import os
+from glob import glob
 
 # 実行ディレクトリ取得
 dirname = os.path.dirname(__file__)
@@ -8,17 +9,22 @@ image_dirname = dirname + "/image/"
 # 画像ファイルが存在しない場合、エラー出力実施
 
 # 画像ファイル取得、テキスト化し出力
-for filename in os.listdir(image_dirname):
-    if filename.endswith(('.png', '.jpg')):
-        img_path = os.path.join(image_dirname, filename)
-        try:
-            img = Image.open(img_path)
-            # Use tesseract to do OCR on the image
-            text = pytesseract.image_to_string(img, lang='jpn')
-            # Print the text from each image
-            print(f'対象ファイル：{filename}:\n{text}\n')
-            print("================================================")
-        except IOError:
-            print(f'ファイル{filename}を開けませんでした。')
-    else:
-        print(f'非画像ファイル{filename}は無視されました。')
+png_files = sorted(glob(image_dirname + "*.png"), key=os.path.getmtime, reverse=False)
+for filename in png_files:
+    img = Image.open(filename)
+    text = pytesseract.image_to_string(img, lang='jpn')
+    print(f'対象ファイル：{filename}:\n{text}\n')
+    print("================================================")
+    # if filename.endswith(('.png', '.jpg')):
+    #     img_path = os.path.join(image_dirname, filename)
+    #     try:
+    #         img = Image.open(img_path)
+    #         # Use tesseract to do OCR on the image
+    #         text = pytesseract.image_to_string(img, lang='jpn')
+    #         # Print the text from each image
+    #         print(f'対象ファイル：{filename}:\n{text}\n')
+    #         print("================================================")
+    #     except IOError:
+    #         print(f'ファイル{filename}を開けませんでした。')
+    # else:
+    #     print(f'非画像ファイル{filename}は無視されました。')
